5b48457354091ba4d39633a2ea6e139563501a7a
Abhinav Dawar <57832927+abhinavdawar@users.noreply.github.com>
2023-04-18 04:01:24 +0000

    All basic done

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from recursion import Recursion
 
-print(Recursion.power(2,2))
+# print(Recursion.power(2,2))
+
+# ans = Recursion.maximumInArray([9, 12, 6, 13, 5], 0)
+# print(ans)
+
+# print(Recursion.minimumInArray([9, 12, 6, 13, 5], 0, 100))
+
+# print(Recursion.find([9, 12, 6, 13, 5], 0, 14))
+
+# print(Recursion.firstIndex([12, 9, 9, 13, 9, 14], 0, 9))
+
+# print(Recursion.allIndex([12, 9, 9, 13, 9, 14], 0, 9))
+
+print(Recursion.subSequence(0, "abc"))
--- recursion.py
+++ recursion.py
@@ -5,20 +5,20 @@ class Recursion:
     if (a > b):
       return
     print(a)
-    Recursion.printIncreasing(a+1, b)
+    printIncreasing(a+1, b)
 
   
   def factorial(x):
     if x == 1:
         return 1
     else:
-        return (x * Recursion.factorial(x-1))
+        return (x * factorial(x-1))
 
   
   def printDecreasing(a, b):
     if(a > b):
       return
-    Recursion.printDecreasing(a+1, b)
+    printDecreasing(a+1, b)
     print(a)
 
   
@@ -26,30 +26,73 @@ class Recursion:
     if (a > b):
       return
     print(a)
-    Recursion.printIncreasingDecreasing(a+1, b)
+    printIncreasingDecreasing(a+1, b)
     print(a)
 
   
   def power(a, b):
     if (b == 0):
       return 1
-    return a * Recursion.power(a, b-1)
+    return a * power(a, b-1)
 
 
   def maximumInArray(arr, idx):
-    pass 
-
+    if (idx == len(arr)):
+      return -1e9
+    maxSF = maximumInArray(arr, idx+1)
+    return max(maxSF, arr[idx])
+    
 
   def minimumInArray(arr, idx):
-    pass 
+    if (idx == len(arr)):
+      return 1e9
+    minSF = minimumInArray(arr, idx+1, ans)
+    return min(minSF, arr[idx])
 
 
-  def find(arr, idx, data)
-  
+  def find(arr, idx, data):
+    if (idx == len(arr)):
+      return -1e9
+    if(arr[idx] == data):
+      return idx
+    return find(arr, idx+1, data)
+    
   
   def firstIndex(arr, idx, data):
-    pass 
+    if(idx == len(arr)):
+      return -1
+    ans = firstIndex(arr, idx+1, data)
+    if(arr[idx] == data):
+      return idx
+    return ans
 
 
   def lastIndex(arr, idx, data):
-    pass
+    if(idx == len(arr)):
+      return -1
+    smallRes = lastIndex(arr, idx+1, data)
+    if(smallRes != -1):
+      return smallRes
+    if(arr[idx] == data):
+      return idx
+    return -1
+
+  
+  # Fetch all the index in array at which particular data is       present
+  def allIndex(arr, idx, data):
+    if(idx == len(arr)):
+      return []
+    myAns = allIndex(arr, idx+1, data)
+    if(arr[idx] == data):
+      myAns.append(idx)
+    return myAns
+
+  
+  def subSequence(idx, str):
+    if(idx == len(str)):
+      return [""]
+    recAns = Recursion.subSequence(idx+1, str)
+    myAns = recAns.copy()
+    for s in recAns:
+      myAns.append(str[idx] + s)
+    return myAns
