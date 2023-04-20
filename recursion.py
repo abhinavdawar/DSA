902907a9ccbd398ea5a0bbb7def4f55583c91d96
abhinavdawar <57832927+abhinavdawar@users.noreply.github.com>
2023-04-20 04:03:01 +0000

    Two ways of recursion

--- recursion.py
+++ recursion.py
@@ -78,7 +78,7 @@ class Recursion:
     return -1
 
   
-  # Fetch all the index in array at which particular data is       present
+  # Fetch all the index in array at which particular data is present
   def allIndex(arr, idx, data):
     if(idx == len(arr)):
       return []
@@ -91,8 +91,60 @@ class Recursion:
   def subSequence(idx, str):
     if(idx == len(str)):
       return [""]
-    recAns = Recursion.subSequence(idx+1, str)
+      
+    recAns = subSequence(idx+1, str)
     myAns = recAns.copy()
     for s in recAns:
       myAns.append(str[idx] + s)
     return myAns
+
+  # Here, ans is type string and res is type array which will store all ans
+  def subSequence_other_way(str, idx, ans, res):
+    if(idx == len(str)):
+      res.add(ans)
+      return 1
+
+    count = 0
+    count += subSequence_other_way(str, idx+1, ans+str[idx], res)
+    count += subSequence_other_way(str, idx+1, ans, res)
+    return count
+
+
+  def mazePath_HVD(sr, sc, er, ec):
+    if(er == sr and ec == sc):
+      return [""]
+
+    myAns = []
+    if(sr+1 <= er):
+      vertical = mazePath_HVD(sr+1, sc, er, ec)
+      for s in vertical:
+        myAns.append("V" + s)
+
+    if(sc+1 <= ec):
+      horizontal = mazePath_HVD(sr, sc+1, er, ec)
+      for s in horizontal:
+        myAns.append("H" + s)
+
+    if(sr+1 <= er and sc+1 <= ec):
+      diagonal = mazePath_HVD(sr+1, sc, er, ec)
+      for s in diagonal:
+        myAns.append("D" + s)
+
+    return myAns
+
+
+  def mazePath_HVD_other_way(sr, sc, er, ec, ans, res):
+    if(sr == er and sc == ec):
+      res.append(ans)
+      return 1
+
+    count = 0
+    if(sr+1 <= er):
+      count += mazePath_HVD_other_way(sr+1, sc, er, ec, ans+"V", res)
+
+    if(sc+1 <= ec):
+      count += mazePath_HVD_other_way(sr, sc+1, er, ec, ans+"H", res)
+
+    if(sr+1 <= er and sc+1 <= ec):
+      count += mazePath_HVD_other_way(sr+1, sc, er, ec, ans+"D", res)
+    return count
